5d9c6abcad01a8f27218b955a8c573b9e019bf0b
mandra71 <mandra71@gmail.com>
2019-05-03 17:22:27 -0600

    Basic layout complete

---
+++ SinglePageProductGrid/AppLogic/ProductGrid.cs
@@ -0,0 +1,34 @@
+﻿using SinglePageProductGrid.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SinglePageProductGrid
+{
+    public partial class ProductGrid : Form
+    {
+        private DataGridView productsGrid = new DataGridView();
+        private Dictionary<int, Product> products = new Dictionary<int, Product>();
+
+        public ProductGrid()
+        {
+            InitializeComponent();
+        }
+
+        private void productSelected(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addProduct_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SinglePageProductGrid/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglePageProductGrid.Models
+{
+    class Product
+    {
+       protected string Title { get; set; }
+       protected string Description { get; set; }
+       protected int Price { get; set; }
+       protected int Quantity { get; set; }
+       protected string ImageLocation { get; set; }
+    }
+}
